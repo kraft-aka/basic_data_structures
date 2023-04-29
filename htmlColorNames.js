2d11733c55a7147194d7d2378a5e584f2e53ca19
kubat <kubat.aiupov@gmail.com>
2023-04-29 19:52:45 +0200

    add htmlColorNames

---
+++ htmlColorNames.js
@@ -0,0 +1,22 @@
+// We have defined a function, htmlColorNames, which takes an array of HTML
+// colors as an argument. Modify the function using splice() to remove the
+// first two elements of the array and add 'DarkSalmon' and 'BlanchedAlmond'
+// in their respective places.
+
+function htmlColorNames(arr) {
+  const startIndex = 0;
+  const amountToDelete = 2;
+  arr.splice(startIndex, amountToDelete, "DarkSalmon", "BlanchedAlmond");
+
+  return arr;
+}
+
+console.log(
+  htmlColorNames([
+    "DarkGoldenRod",
+    "WhiteSmoke",
+    "LavenderBlush",
+    "PaleTurquoise",
+    "FireBrick",
+  ])
+);
